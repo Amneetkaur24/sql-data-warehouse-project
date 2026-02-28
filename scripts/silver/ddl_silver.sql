58059b3aa9d664551149c9c7bcc6ca487716a30c
Amneet Kaur <amneet1224@gmail.com>
2026-02-27 23:26:40 -0500

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -23,6 +23,7 @@ IF OBJECT_ID ('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info(
 prd_id INT,
+cat_id NVARCHAR(50),
 prd_key NVARCHAR(50),
 prd_nm NVARCHAR(50),
 prd_cost INT,
@@ -37,9 +38,9 @@ CREATE TABLE silver.crm_sales_details(
 sls_ord_num NVARCHAR(50), 
 sls_prd_key NVARCHAR(50),
 sls_cust_id INT,
-sls_order_dt INT,
-sls_ship_dt INT, 
-sls_due_dt INT,
+sls_order_dt DATE,
+sls_ship_dt DATE, 
+sls_due_dt DATE,
 sls_sales INT,
 sls_quantity INT,
 sls_price INT,
@@ -64,7 +65,7 @@ IF OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2(
 id NVARCHAR(50),
-cat NVARCHAR(50),
+cat_id NVARCHAR(50),
 subcat NVARCHAR (50),
 maintenance NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE());
