291dce60a83edee5dfd5778fafa4ca10db5e6703
Amneet Kaur <amneet1224@gmail.com>
2026-02-27 23:31:37 -0500

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -167,7 +167,7 @@ BEGIN
 	PRINT '>> Truncating the Table: silver.erp_px_cat_g1v2';
 	TRUNCATE TABLE silver.erp_px_cat_g1v2;
 	PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-	INSERT INTO silver.erp_px_cat_g1v2(id, cat, subcat, maintenance)
+	INSERT INTO silver.erp_px_cat_g1v2(id, cat_id, subcat, maintenance)
 	SELECT id, cat, subcat, maintenance 
 	FROM bronze.erp_px_cat_g1v2
 	SET @end_time= GETDATE();
